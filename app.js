f64e9876b78af6bf9f5465e5867f8b0e4c07667b
Krish Saluja <programming.kris9801@gmail.com>
2026-05-08 14:23:49 -0400

    Update app.js
    Fix: Previously incorrect commit

--- app.js
+++ app.js
@@ -1,8 +1,3 @@
-const SUPABASE_URL      = 'https://ootthvupwgluzqbjotsf.supabase.co';
-const SUPABASE_ANON_KEY = 'sb_publishable_rg6GAbkJPBsGHzkw_KGRcw_eKeC33sB';
-
-const db = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
-
 class MultiUserRoutinePlanner {
     constructor() {
         this.userKey     = null;
