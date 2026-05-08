0b89f8824e99cae220cbdc72442998346e8ec520
Krish Saluja <programming.kris9801@gmail.com>
2026-05-08 14:47:57 -0400

    Update supabase.js
    Fix: Wire to correct database

--- config/supabase.js
+++ config/supabase.js
@@ -1,4 +1,4 @@
-const SUPABASE_URL      = 'https://hcwqlbjnweatwmvkcape.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imhjd3FsYmpud2VhdHdtdmtjYXBlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI1MjM1ODMsImV4cCI6MjA2ODA5OTU4M30.oItoCmICG44IJ_Iai2wE1GEgK3pQW-PJbuk8TCuf3O0';
+const SUPABASE_URL      = 'https://ootthvupwgluzqbjotsf.supabase.co';
+const SUPABASE_ANON_KEY = 'sb_publishable_rg6GAbkJPBsGHzkw_KGRcw_eKeC33sB';
 
 const db = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
